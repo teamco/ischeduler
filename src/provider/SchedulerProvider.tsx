668b6587a6d9531e076c1caebb169907257085ad
Volodymyr Tkach <teamco@gmail.com>
2026-03-19 08:57:16 +0200

    chore: fix linter

--- src/provider/SchedulerProvider.tsx
+++ src/provider/SchedulerProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { SchedulerContext } from './SchedulerContext';
 import { createTranslationFn, defaultTranslations } from '../i18n';
 import type { SchedulerTranslations } from '../i18n';
